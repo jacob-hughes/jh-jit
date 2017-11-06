bdc375a42cc09c1c2c48bf0745f5a6c655793be7
Jake <jacob.hughes@kcl.ac.uk>
2017-11-06 01:50:33 +0000

    Included a compiler cli which outputs a bytecode file given a source program with the .jh file extension.

---
+++ compiler.py
@@ -0,0 +1,34 @@
+# vim: ai ts=4 sts=4 et sw=4
+# -*- coding: utf-8 -*-
+import sys
+from bytecode.parser import parse_input
+from bytecode.gen import generate_bytecode
+
+def usage():
+    print >> sys.stderr, 'Usage: compiler.py filename.jh'
+    sys.exit(1)
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+
+    filename = sys.argv[1]
+    if not filename.endswith('.jh'):
+        usage()
+
+    with open(filename) as f:
+        source_code = f.read()
+
+    ast = parse_input(source_code)
+    bytecode = generate_bytecode(ast)
+
+    outname = filename[:-len('.jh')]
+    with open(outname, 'wb') as f:
+        for op in bytecode:
+            f.write('%s\n' % op)
+    print '%s successfully compiled.' % outname
+
+if __name__ == '__main__':
+    main()
+
+
